2c4096531e46825cb8b4c42a670296756fd4c9b4
github username <github email address>
2023-09-27 09:04:41 -0400

    initial commit

--- lib/debug.py
+++ lib/debug.py
@@ -4,5 +4,7 @@
 from models.__init__ import CONN, CURSOR
 import ipdb
 
+def reset_database():
+    pass
 
 ipdb.set_trace()
---
+++ lib/seed.py
@@ -0,0 +1,13 @@
+
+from models.__init__ import CONN, CURSOR
+
+# import models
+
+def seed_database():
+    # drop and create tables for models
+    # Create seed data
+    pass
+
+
+seed_database()
+print("Seeded database")
